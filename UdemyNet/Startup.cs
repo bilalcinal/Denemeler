00a90e67f3a23c436b61ac4d5d5e7a4f5b6b45b6
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2022-06-17 14:19:25 +0300

    Startup.cs e defaults parametresi eklendi ve gitignor kaldırıldı

---
+++ UdemyNet/Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UdemyNet.Middlewares;
+
+namespace UdemyNet
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // Controller => View
+            // HomeController => home
+            // Index() => index.cshtml
+
+            // Localhost/home/Index
+            
+             app.UseRouting();
+             app.UseEndpoints(endpoint=>
+             {
+                endpoint.MapControllerRoute(
+                    name:"default",
+                    pattern:"{Controller}/{Action}",
+                    defaults: new {Controller="home",Action="Index"}
+                );
+             }
+
+             );
+            // app.UseMiddleware<ResponseEditingMiddleware>();
+            // IApplicationBuilder applicationBuilder = app.UseMiddleware<RequestEdittingMiddleware>();
+
+           // app.UseEndpoints(endpoints =>
+           // {
+          //      endpoints.MapGet("/", async context =>
+          //      {
+          //          await context.Response.WriteAsync("Hello World!");
+          //     });
+          //  });
+        }
+    }
+}
