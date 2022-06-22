9cbdde319fd35ba0588311684e36532ae2647202
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2022-06-22 16:11:20 +0300

    bootstrap eklendi ama çalışmıyor sebep bilinmiyor

--- UdemyNet/Startup.cs
+++ UdemyNet/Startup.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Primitives;
 using UdemyNet.Middlewares;
 
 namespace UdemyNet
@@ -35,6 +38,11 @@ namespace UdemyNet
             
              app.UseRouting();
              app.UseStaticFiles();
+             app.UseStaticFiles(new StaticFileOptions
+             {
+                 RequestPath = "/nodem_modules",
+                 FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "node_modules"))
+             });
              app.UseEndpoints(endpoints=>
            {  
             
@@ -66,4 +74,6 @@ namespace UdemyNet
           //  });
         }
     }
+
+   
 }
