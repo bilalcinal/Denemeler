f0148e1c9b66ae9a9c17a0131c8b18438b4b25f6
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2022-07-04 15:42:51 +0300

    File create oluşuturuldu

--- UdemyNet/Controllers/FileController.cs
+++ UdemyNet/Controllers/FileController.cs
@@ -11,6 +11,21 @@ namespace UdemyNet.Controllers
            
             var files = directoryInfo.GetFiles();
             return View(files);
+        
+        }
+
+        public IActionResult  Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string fileName)
+        { 
+            FileInfo fileInfo = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(),"wwwroot","files", fileName));
+
+             if(!fileInfo.Exists)
+                fileInfo.Create();
+            return RedirectToAction("List");
         }
     }
 }
