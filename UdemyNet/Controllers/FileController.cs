1c78e00b6051580acb2bfcb2bbfd3c25d53fbabb
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2022-07-12 14:51:26 +0300

    File Upload

--- UdemyNet/Controllers/FileController.cs
+++ UdemyNet/Controllers/FileController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UdemyNet.Controllers
@@ -38,6 +39,29 @@ namespace UdemyNet.Controllers
             writer.Write("Merhaba ben Bilal");
             writer.Close();
             return RedirectToAction("list");
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult upload(IFormFile formFile)
+        { 
+          if(formFile.ContentType == "image/png/jpg")
+          {
+          var ext = Path.GetExtension(formFile.FileName);
+          var path = Directory.GetCurrentDirectory()+ "/wwwroot" +"/Images/" + Guid.NewGuid()+ ext;
+          FileStream stream = new FileStream(path,FileMode.Create);
+          formFile.CopyTo(stream);
+          TempData["message"] = "Dosya Başarılı bir şekilde yüklendi";
+          }
+          else
+          {
+            TempData["message"] = "Dosya istenilen türde değil";
+          }
+           return RedirectToAction("List");
         }
     }
 }
