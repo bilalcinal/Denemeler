350355393ca7fd4d4e5c43f7d24e33d15122b62f
bilalcinal <73084109+bilalcinal@users.noreply.github.com>
2022-07-26 17:28:20 +0300

    Views componentleri renklendirme

--- UdemyNet/ViewComponents/News.cs
+++ UdemyNet/ViewComponents/News.cs
@@ -5,10 +5,15 @@ namespace UdemyNet.ViewComponents
 {
     public class News : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(string color="default")
         {   
             var List = NewsContext.List;
-            return View(List);
+            if (color==default)
+                return View(List);
+            else if(color=="red")
+               return View("red", List);
+            else 
+               return View("blue", List);
         }
     }
 }
